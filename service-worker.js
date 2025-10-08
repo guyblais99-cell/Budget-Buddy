5ef61d499adf8f4a5a830f496cfbc560ea73355a
guyblais99-cell <guyblais.99@gmail.com>
2025-10-07 19:39:15 -0600

    pwa

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+// A minimal service worker to make the app installable.
+self.addEventListener('fetch', (event) => {
+  // This empty fetch event handler is enough to make the app installable.
+});
